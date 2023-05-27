a96c4cfaabc72615c2912eda248c930893cca190
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2023-05-27 18:31:36 +0200

    build: configure runtime envs for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     exclude: ["**/node_modules/", "**/dist/", ".idea/", ".vscode/", ".git/", ".cache/"],
+    environmentMatchGlobs: [
+      ["**/tests/**/*.edge.{test,spec}.{ts,tsx}", "edge-runtime"],
+      ["**/tests/**/*.{test,spec}.{ts,tsx}", "node"],
+    ],
     passWithNoTests: true,
   },
 });
