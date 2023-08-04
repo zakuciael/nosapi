e9c40e3f8ad61ffd21586ec9f1a2c1823c0ab16d
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2023-08-04 10:42:51 +0200

    chore: replace shuttle with plain rocket server

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -3,13 +3,11 @@ use rocket_include_static_resources::static_resources_initializer;
 
 mod routes;
 
-#[shuttle_runtime::main]
-async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build()
+#[rocket::launch]
+async fn rocket() -> _ {
+    rocket::build()
         .attach(static_resources_initializer!(
             "favicon" => "assets/favicon.ico"
         ))
-        .mount("/", routes![routes::serve::favicon, routes::index::index]);
-
-    Ok(rocket.into())
+        .mount("/", routes![routes::serve::favicon, routes::index::index])
 }
