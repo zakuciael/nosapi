be10d09a9f8f65a9a88162fbe4db5488611286de
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-12-31 15:29:29 +0100

    refactor(nosapi-blackbox): remove `generate_game_string()` utility

--- crates/nosapi_blackbox/src/utils.rs
+++ crates/nosapi_blackbox/src/utils.rs
@@ -2,7 +2,6 @@ use rand::distributions::{Distribution, Uniform};
 use rand::Rng;
 
 const VECTOR_STRING_LENGTH: usize = 100;
-const GAME_STRING_LENGTH: usize = 3;
 
 fn random_ascii_generator() -> Uniform<u8> {
   Uniform::new(32, 126)
@@ -30,8 +29,4 @@ pub fn random_ascii_string(length: usize) -> String {
 
 pub fn generate_vector_string() -> String {
   random_ascii_string(VECTOR_STRING_LENGTH)
-}
-
-pub fn generate_game_string() -> String {
-  random_ascii_string(GAME_STRING_LENGTH)
 }
