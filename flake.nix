b90ef798bcbf35ccc37324c74f9dd674336f691c
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2025-01-01 18:28:17 +0100

    ci: limit flake systems to `x86_64-linux`

--- flake.nix
+++ flake.nix
@@ -26,11 +26,7 @@
   outputs =
     { flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "i686-linux"
-      ];
+      systems = [ "x86_64-linux" ];
       debug = true;
 
       perSystem =
